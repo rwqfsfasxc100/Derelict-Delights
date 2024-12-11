fb4c27f3539caed0c912c3fa98ec5d5bba01787b
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2024-12-11 09:15:00 -0800

    Update RescueOperation.gd

--- story/RescueOperation.gd
+++ story/RescueOperation.gd
@@ -1,1 +1,5 @@
+extends Node
 
+export (float, 0, 1, 0.01) var randomChance = 1.0
+export (float, 0, 1, 0.01) var minimumChance = 0.1
+export (float, 100000.0, 2e+07, 100000.0) var moneyCeiling = 1e+07
