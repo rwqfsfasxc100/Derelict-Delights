a74c93613b8465b61216481a1975208666a72cce
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2024-12-12 22:25:16 -0800

    Update Shipyard.gd

--- ships/Shipyard.gd
+++ ships/Shipyard.gd
@@ -5,93 +5,96 @@ var DDUsedShips = {
 		{
 		"weaponSlot":{
 			"right":{
-				"type":"SYSTEM_EINAT"
-			}, 
+				"type":"SYSTEM_MWG"
+				}, 
 			"left":{
-				"type":"SYSTEM_EINAT"
-			}
-		}, 
+				"type":"SYSTEM_MWG"
+				}
+			}, 
 		"reactor":{
 			"power":8.0
-		}, 
-		"ammo":{
-			"capacity":1000.0, 
-			"initial":1000.0, 
-		}, 
-		"drones":{
-			"capacity":0.0, 
-			"initial":0.0
-		}, 
-		"fuel":{
-			"capacity":80000.0, 
-			"initial":80000.0, 
-		}, 
-		"capacitor":{
-			"capacity":500.0, 
-		}, 
-		"turbine":{
-			"power":200.0, 
-		}, 
-		"shielding":{
-			"emp":0, 
-		}, 
-		"cargo":{
-			"equipment":"SYSTEM_CARGO_STANDARD"
-		}, 
-		"autopilot":{
-			"type":"SYSTEM_AUTOPILOT_MK3"
-		}, 
-		"propulsion":{
-			"main":"SYSTEM_MAIN_ENGINE_PNTR", 
-			"rcs":"SYSTEM_THRUSTER_NDSTR"
-		}
-	}, 
-	{
-		"weaponSlot":{
-			"right":{
-				"type":"SYSTEM_NANI"
 			}, 
-			"left":{
-				"type":"SYSTEM_NANI"
-			}
-		}, 
-		"reactor":{
-			"power":12.0
-		}, 
 		"ammo":{
 			"capacity":0.0, 
 			"initial":0.0, 
-		}, 
+			}, 
 		"drones":{
 			"capacity":0.0, 
 			"initial":0.0
-		}, 
+			}, 
 		"fuel":{
 			"capacity":80000.0, 
 			"initial":80000.0, 
-		}, 
+			}, 
 		"capacitor":{
-			"capacity":500.0, 
-		}, 
+			"capacity":1000.0, 
+			}, 
 		"turbine":{
 			"power":200.0, 
-		}, 
+			}, 
 		"shielding":{
 			"emp":0, 
-		}, 
+			}, 
 		"cargo":{
 			"equipment":"SYSTEM_CARGO_STANDARD"
-		}, 
+			}, 
 		"autopilot":{
 			"type":"SYSTEM_AUTOPILOT_MK3"
-		}, 
+			}, 
 		"propulsion":{
 			"main":"SYSTEM_MAIN_ENGINE_PNTR", 
+			"rcs":"SYSTEM_THRUSTER_NDSTR"
+			}
+		}, 
+		{
+		"weaponSlot":{
+			"right":{
+				"type":"SYSTEM_CL600P"
+				}, 
+			"left":{
+				"type":"SYSTEM_CL600P"
+				}
+			}, 
+		"reactor":{
+			"power":30.0
+			}, 
+		"ammo":{
+			"capacity":0.0, 
+			"initial":0.0, 
+			}, 
+		"drones":{
+			"capacity":0.0, 
+			"initial":0.0
+			}, 
+		"fuel":{
+			"capacity":80000.0, 
+			"initial":80000.0, 
+			}, 
+		"capacitor":{
+			"capacity":1500.0, 
+			}, 
+		"turbine":{
+			"power":500.0, 
+			}, 
+		"aux":{
+			"SYSTEM_AUX_SMES_MK2"
+			}, 
+		"shielding":{
+			"emp":0, 
+			}, 
+		"cargo":{
+			"equipment":"SYSTEM_CARGO_STANDARD"
+			}, 
+		"autopilot":{
+			"type":"SYSTEM_AUTOPILOT_MK3"
+			}, 
+		"propulsion":{
+			"main":"SYSTEM_MAIN_ENGINE_BWMT535", 
 			"rcs":"SYSTEM_THRUSTER_MA150HO"
-		}
+			}
 		}
 	]
 }
 
 func _ready():
-  usedShipConfigs.merge(DDUsedShips)
+  usedShipConfigs.merge(DDUsedShips, true)
