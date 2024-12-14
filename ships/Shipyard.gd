43f309c646b186c6d49e8e42088bd895aeaab1ad
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2024-12-13 20:24:28 -0800

    Update Shipyard.gd

--- ships/Shipyard.gd
+++ ships/Shipyard.gd
@@ -268,6 +268,77 @@ var DDUsedShips = {
 				"type": "SYSTEM_EXMONO-R"
 			}
 		}
+	},
+	{
+		"ammo": {
+			"capacity": 0,
+			"initial": 0
+		},
+		"autopilot": {
+			"type": "SYSTEM_AUTOPILOT_RTYPE"
+		},
+		"aux": {
+			"power": "SYSTEM_NONE"
+		},
+		"capacitor": {
+			"capacity": 1500
+		},
+		"cargo": {
+			"equipment": "SYSTEM_CARGO_MPUFSO"
+		},
+		"drones": {
+			"capacity": 50000.0,
+			"initial": 50000.0
+		},
+		"fuel": {
+			"capacity": 500000,
+			"initial": 500000
+		},
+		"hud":{
+			"type": "SYSTEM_HUD_AT225"
+		},
+    		"drone": {
+			"scanner": "SYSTEM_RD_SPECTROMETER"
+		},
+		"lidar": {
+			"type": "SYSTEM_LIDAR_DOPPLER"
+		},
+		"propulsion": {
+			"main": "SYSTEM_MAIN_ENGINE_K44",
+			"rcs": "SYSTEM_THRUSTER_NDSTR"
+		},
+		"reactor": {
+			"power": 30
+		},
+		"turbine": {
+			"power": 500
+		},
+		"weaponSlot": {
+			"leftBay1":{
+				"type": "SYSTEM_DND_HAUL"
+			},
+			"leftBay2": {
+				"type": "SYSTEM_EXSTORAGE-L"
+			},
+			"leftBay3": {
+				"type": "SYSTEM_EXSTORAGE-L"
+			},
+			"middleLeft": {
+				"type": "SYSTEM_PDMWG"
+			},
+			"middleRight": {
+				"type": "SYSTEM_PDMWG"
+			},
+			"rightBay1": {
+				"type": "SYSTEM_DND_HAUL"
+			},
+			"rightBay2": {
+				"type": "SYSTEM_EXSTORAGE-R"
+			},
+			"rightBay3": {
+				"type": "SYSTEM_EXSTORAGE-R"
+			}
+		}
 	}
 	],
 	"OCP209": [
@@ -311,6 +382,65 @@ var DDUsedShips = {
 		},
 		"reactor":{
 			"power":30
+		},
+		"turbine":{
+			"power":200
+		},
+		"weaponSlot":{
+			"mainLeft":{
+				"type":"SYSTEM_SALVAGE_ARM"
+			},
+			"mainRight":{
+				"type":"SYSTEM_SALVAGE_ARM"
+			},
+			"middleLeft":{
+				"type":"SYSTEM_DND_HAUL"
+			},
+			"middleRight":{
+				"type":"SYSTEM_ACL200P"
+			}
+		}
+	},
+	{
+		"ammo":{
+			"capacity":0,
+			"initial":0
+		},
+		"autopilot":{
+			"type":"SYSTEM_AUTOPILOT_MK3"
+		},
+		"aux":{
+			"power":"SYSTEM_AUX_SMES"
+		},
+		"capacitor":{
+			"capacity":1000
+		},
+		"cargo":{
+			"equipment":"SYSTEM_CARGO_MPUSML"
+		},
+		"drone":{
+			"scanner":"SYSTEM_RD_SPECTROMETER"
+		},
+		"drones":{
+			"capacity":10000,
+			"initial":10000
+		},
+		"fuel":{
+			"capacity":200000,
+			"initial":200000
+		},
+		"hud":{
+			"type":"SYSTEM_HUD_OCP209"
+		},
+		"lidar":{
+			"type":"SYSTEM_LIDAR_DOPPLER_HIRES"
+		},
+		"propulsion":{
+			"main":"SYSTEM_MAIN_ENGINE_DFMPD2205",
+			"rcs":"SYSTEM_THRUSTER_K44"
+		},
+		"reactor":{
+			"power":40
 		},
 		"turbine":{
 			"power":200
