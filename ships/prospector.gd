431aa67a9f2627bd9df197e9724974ee9e4392fe
rwqfsfasxc100 <123430291+rwqfsfasxc100@users.noreply.github.com>
2024-12-15 07:41:30 -0800

    Update prospector.gd

--- ships/prospector.gd
+++ ships/prospector.gd
@@ -1,7 +1,7 @@
 extends "res://ships/Shipyard.gd"
 
 func _ready():
-	usedShipConfigs.append_array(PROSPECTOR, true)
+	usedShipConfigs.append_array(PROSPECTOR)
 
 var PROSPECTOR = {
 	"PROSPECTOR":[
