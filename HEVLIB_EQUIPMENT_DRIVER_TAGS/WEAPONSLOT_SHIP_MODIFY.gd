016a1f14365218842368bc776457e67a3d9fde26
__hev <123430291+rwqfsfasxc100@users.noreply.github.com>
2025-11-07 23:45:19 -0800

    fixed container positioning on titans

---
+++ HEVLIB_EQUIPMENT_DRIVER_TAGS/WEAPONSLOT_SHIP_MODIFY.gd
@@ -0,0 +1,90 @@
+extends Node
+
+const WEAPONSLOT_SHIP_MODIFY = {
+	"AT225":{
+		"leftBay1":{
+			"SYSTEM_EXSUPER-L":[
+				{
+					"property":"position",
+					"value":"Vector2( -35, 190 )"
+				}
+			],
+			"SYSTEM_EXBIG-L":[
+				{
+					"property":"position",
+					"value":"Vector2( -10, 185 )"
+				}
+			],
+		},
+		"rightBay1":{
+			"SYSTEM_EXSUPER-R":[
+				{
+					"property":"position",
+					"value":"Vector2( 35, 190 )"
+				}
+			],
+			"SYSTEM_EXBIG-R":[
+				{
+					"property":"position",
+					"value":"Vector2( 10, 185 )"
+				}
+			],
+		},
+		"leftBay2":{
+			"SYSTEM_EXSUPER-L":[
+				{
+					"property":"position",
+					"value":"Vector2( -35, 190 )"
+				}
+			],
+			"SYSTEM_EXBIG-L":[
+				{
+					"property":"position",
+					"value":"Vector2( -10, 185 )"
+				}
+			],
+		},
+		"rightBay2":{
+			"SYSTEM_EXSUPER-R":[
+				{
+					"property":"position",
+					"value":"Vector2( 35, 190 )"
+				}
+			],
+			"SYSTEM_EXBIG-R":[
+				{
+					"property":"position",
+					"value":"Vector2( 10, 185 )"
+				}
+			],
+		},
+		"leftBay3":{
+			"SYSTEM_EXSUPER-L":[
+				{
+					"property":"position",
+					"value":"Vector2( -35, 190 )"
+				}
+			],
+			"SYSTEM_EXBIG-L":[
+				{
+					"property":"position",
+					"value":"Vector2( -10, 185 )"
+				}
+			],
+		},
+		"rightBay3":{
+			"SYSTEM_EXSUPER-R":[
+				{
+					"property":"position",
+					"value":"Vector2( 35, 190 )"
+				}
+			],
+			"SYSTEM_EXBIG-R":[
+				{
+					"property":"position",
+					"value":"Vector2( 10, 185 )"
+				}
+			],
+		},
+	}
+}
